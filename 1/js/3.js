2b8808a84eea966830f8d0df9d6fdfdc0938d4e4
qazaqlife <8gg001@gmail.com>
2018-12-10 02:27:59 +0600

    new comments for scripts

--- 1/js/3.js
+++ 1/js/3.js
@@ -1,10 +1,26 @@
+// Задает body новый стиль фона
 document.body.setAttribute('style', 'background: red;');
 
+// Создает prompt помещая в $get_back и выводит его...
 var get_back = prompt('Get back');
 get_back;
 
+// Если $get_back равен 1, то цвет фона меняется
 if(get_back == 1) {
 	document.body.setAttribute('style', 'background: white;');
-} else {
+}
+
+// Создаем ссылку
+// Помещаем в нее текст
+// Добавляем ее на страницу
+var link = document.createElement('a');
+link.textContent = 'Google';
+document.body.appendChild(link);
+
+// После клика по $link запускаем функцию go
+// Открывает новую вкладку
+link.addEventListener('click', go);
+function go() {
 	window.open('https://google.com');
 }
+
