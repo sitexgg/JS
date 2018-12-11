18489bc462c048e15601e8b55794492d2d42c1c1
qazaqlife <8gg001@gmail.com>
2018-12-11 23:25:01 +0600

    new Scripts security

--- 2/js/1.js
+++ 2/js/1.js
@@ -1,12 +1,23 @@
 var el_animation = document.getElementById('animation');
 
-el_animation.onclick = function() {
+el_animation.addEventListener('click', go_animation);
+ function go_animation () {	
 	this.style.backgroundColor = 'red';
 	this.style.fontSize = '4em';
 	this.style.color = 'white';
 	this.style.boxShadow = '1px 3px 10px black';
 	this.style.textShadow = '1px 3px 10px black';
+	document.body.style.backgroundColor = 'yellow';
+	document.body.style.transition = 'background-color 7s';
 }
+
+el_animation.addEventListener('contextmenu', go_context);
+function go_context () {
+	this.style.backgroundColor = 'rgb(22,33,11)';
+	this.textContent = 'Very Good!';
+}
+
+
 
 var el_h1 = document.querySelector('h1'); 
 el_h1.className = 'new_bg';
