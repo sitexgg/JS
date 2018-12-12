144aa7270d12f97e6b98ee5cbced0cb08bf7e305
qazaqlife <8gg001@gmail.com>
2018-12-12 11:42:43 +0600

    new Scripts

--- 2/js/3.js
+++ 2/js/3.js
@@ -5,3 +5,16 @@ mouse.addEventListener('mousemove', mA);
 function mA () {
 	this.style.display = 'none';
 }
+
+var $but = document.body.querySelector('button');
+
+$but.addEventListener('click', newEl);
+function newEl () {
+	var $hr = document.createElement('hr');
+	var $link = document.createElement('pre');
+		$link.textContent = 'raz\n =\n dva\n';
+			$link.setAttribute('style', 'text-align: center; margin: 2em; font-family: sans-serif;');
+			document.body.appendChild($link);
+			document.body.appendChild($hr);
+		mouse.removeEventListener('mousemove', mA);		
+}
