45a7965869f050e24fd95f4351bcc7daf556aa49
qazaqlife <8gg001@gmail.com>
2018-12-21 04:01:16 +0600

    upgrade preloader

--- Libs/preloader/preloader.js
+++ Libs/preloader/preloader.js
@@ -1,11 +1,21 @@
  // Preloader
 var divPreloader = document.createElement('div');
 	divPreloader.setAttribute('class', 'preloader');
+ // Text loader
+var textLoader = document.createElement('p');
+	textLoader.textContent = 'WebNet';
+	textLoader.style.cssText = 'color: white; text-align: center; font-size: 6em; text-shadow: 0 0 25px blue;';
+ // Logo 
+var imgLoader = document.createElement('img');
+	imgLoader.style.cssText = 'margin: 0 auto; display: block;';
+	imgLoader.setAttribute('src', 'mini_logo.png');
  // Loader
 var divLoader = document.createElement('div');
 	divLoader.setAttribute('class', 'loader');
  // Append elements
 document.body.appendChild(divPreloader);
+divPreloader.appendChild(textLoader);
+textLoader.appendChild(imgLoader);
 divPreloader.appendChild(divLoader);
  // Loading
 document.body.onload = function() {
@@ -14,10 +24,10 @@ document.body.onload = function() {
  			if(!preloader.classList.contains('done')) {
  				preloader.style.cssText = 'opacity: 0; visibility: hidden;';
  			}
- 		}, 500);
+ 		}, 999500);
  	}
  // Delete preloader
-setTimeout(dd, 2000);
+setTimeout(dd, 9992000);
 function dd() {
 	document.body.removeChild(divPreloader);
 }
